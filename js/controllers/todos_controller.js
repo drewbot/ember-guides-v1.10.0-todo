0c2562b7899c777cb6c7028994c51f384ff07a46
Drew Botka <hellodrewbot@gmail.com>
2015-05-23 10:15:09 -0400

    made number of remaining items dynamic by adding helpers in template and remaining and inflection properties to the todos controller

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,4 +1,5 @@
 Todos.TodosController = Ember.ArrayController.extend({
+  
   actions: {
     createTodo: function() {
       // Get the todo title value set by the "New Todo" text field
@@ -17,5 +18,15 @@ Todos.TodosController = Ember.ArrayController.extend({
       // Save the new model
       todo.save();
     }
-  }
+  },
+
+  remaining: function() {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
+
 });
