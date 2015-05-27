7a1682cdbc93c98d24535b095b2cd6ad742f305f
Drew Botka <hellodrewbot@gmail.com>
2015-05-26 20:22:34 -0400

    added removeTodo action to the delete button for each model instance

--- js/views/edit_todo_view.js
+++ js/views/edit_todo_view.js
@@ -1,7 +1,10 @@
+// Create an input element
 Todos.EditTodoView = Ember.TextField.extend({
+	// focus on this input element when it appears
   didInsertElement: function() {
     this.$().focus();
   }
 });
-
+// Store as a handlebars helper
 Ember.Handlebars.helper('edit-todo', Todos.EditTodoView);
+// In index.html replace the input elment with this custom component
