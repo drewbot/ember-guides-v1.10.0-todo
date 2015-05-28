75a115ebcf8f01155723b87e0d4b9ec0cc351f90
Drew Botka <hellodrewbot@gmail.com>
2015-05-27 20:32:00 -0400

    included active route and link-to helper, reusing todos-index template with filtered models by isCompleted state

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes will go here later
+    this.route('active');
   });
 });
 
@@ -10,8 +11,26 @@ Todos.TodosRoute = Ember.Route.extend({
   }
 });
 
+// Nested route (NOT included in Router above)
+// shows the list of todos by default, without a url change
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
+  }
+});
+
+// Nested route (IS included in Router above)
+// Transition to show all active todos
+Todos.TodosActiveRoute = Ember.Route.extend({
+	// The model data for this route is the collection of todos whose isCompleted property is false.
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+
+  // reuse the existing todos/index template and corresponding controller
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
